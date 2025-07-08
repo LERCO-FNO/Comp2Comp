7795431ba7257ce82a2fc63f79342cac9c79d665
Vojtěch-Benda <vojtech.benda@atlas.cz>
2025-07-08 14:07:58 +0200

    added ability to filter undesired series

---
+++ filter_series.py
@@ -0,0 +1,75 @@
+import os
+import shutil
+import argparse
+from pathlib import Path
+import pydicom
+
+
+def get_args():
+    parser = argparse.ArgumentParser(prog="filter_series.py", description="filter series")
+    parser.add_argument("in_directory", type=str, help="input directory containing dicom files")
+    parser.add_argument("-od", "--out_directory", type=str, help="output directory of filtered studies", default="./outdir")
+
+    return parser.parse_args()
+
+
+def filter_files(ds):
+    # found = False
+    series_desc = ["report", "dose", "protocol", "topogram", "scout", "coronal", "sagital"]
+    if any(sub in ds.SeriesDescription.lower() for sub in series_desc):
+        return True
+    
+    if ds.SliceThickness > 1:
+        return True
+
+    if not any("primary" in image_type.lower() for image_type in list(ds.ImageType)):
+        return True
+    
+    # return found 
+
+
+
+args = get_args()
+
+in_directory = Path(args.in_directory)
+study_dirs = os.listdir(in_directory)
+out_directory = Path(args.out_directory)
+os.makedirs(out_directory, exist_ok=True)
+
+count = 0
+for root, _, files in os.walk(in_directory):
+    if len(files) <= 0 or "DICOMDIR" in files:
+        continue
+    
+    series_paths: dict[str, list[Path]] = {}
+    patient_id = None
+    study_uid = None
+    for file in files:
+        old_path = Path(root, file)
+        ds = pydicom.dcmread(Path(root, file))
+        if filter_files(ds):
+            continue
+
+        if not patient_id:
+            patient_id = ds.PatientID
+
+        if not study_uid:
+            study_uid = ds.StudyInstanceUID
+
+        if not ds.SeriesInstanceUID in series_paths:
+            series_paths[ds.SeriesInstanceUID] = []
+            
+        series_paths[ds.SeriesInstanceUID].append(old_path)
+
+
+    for series_uid, paths in series_paths.items():
+        series_dir = Path(out_directory, f"{patient_id}_{study_uid}", series_uid)
+        os.makedirs(series_dir, exist_ok=True)
+        
+        for src_path in paths:
+            dest_path = Path(series_dir, src_path.stem)
+            shutil.move(src_path, dest_path)
+        print(f"moved {series_uid}: {len(paths)} files to {series_dir}")
+        
+
+        
