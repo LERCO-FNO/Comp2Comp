73c4feff1ddeb2c0b0f602ea827a46a8b11a0765
Vojtěch-Benda <vojtech.benda@atlas.cz>
2025-07-15 13:07:17 +0200

    now uses slice_thickness as filter criteria

--- filter_series.py
+++ filter_series.py
@@ -3,73 +3,75 @@ import shutil
 import argparse
 from pathlib import Path
 import pydicom
+import SimpleITK as sitk
+from typing import Any
 
 
 def get_args():
     parser = argparse.ArgumentParser(prog="filter_series.py", description="filter series")
     parser.add_argument("in_directory", type=str, help="input directory containing dicom files")
     parser.add_argument("-od", "--out_directory", type=str, help="output directory of filtered studies", default="./outdir")
-    parser.add_argument("--append_study_uid", action="store_true", help="append StudyInstanceUID to patientID in directory name", default=False)
-    # parser.add_argument("--series_dir_name", type=str, help="use SeriesDescription or SeriesInstanceUID as names for directories", choices=("desc", "uid"))
 
     return parser.parse_args()
 
 
-def filter_files(ds):
-    series_desc = ["report", "dose", "protocol", "topogram", "scout", "coronal", "sagital"]
-    if any(sub in ds.SeriesDescription.lower() for sub in series_desc):
-        return True
-    
-    slice_thickness = getattr(ds, "SliceThickness", None)
-    if slice_thickness is None or slice_thickness > 1.0:
-        return True
-
-    if not any("primary" in image_type.lower() for image_type in list(ds.ImageType)):
-        return True
-
-
 args = get_args()
+reader = sitk.ImageFileReader()
 
 in_directory = Path(args.in_directory)
-study_dirs = os.listdir(in_directory)
-out_directory = Path(args.out_directory)
-os.makedirs(out_directory, exist_ok=True)
 
 for root, _, files in os.walk(in_directory):
-    if len(files) <= 0 or "DICOMDIR" in files:
+    if not files or "DICOMDIR" in files:
         continue
     
-    series_paths: dict[str, list[Path]] = {}    
-    ds = pydicom.dcmread(Path(root, files[0]))
-    patient_id = getattr(ds, "PatientID", None)
-    study_uid = getattr(ds, "StudyInstanceUID", None)
+    filtered_series: dict[str, dict[str, Any]] = {}
+            
+    series_ids = sitk.ImageSeriesReader_GetGDCMSeriesIDs(root)
     
-    for file in files:
-        old_path = Path(root, file)
-        ds = pydicom.dcmread(old_path)
+    for series_id in series_ids:
+        filenames = sitk.ImageSeriesReader_GetGDCMSeriesFileNames(root, series_id)
+        reader.SetFileName(filenames[0])
+        reader.ReadImageInformation()
         
-        if filter_files(ds):
-            continue
-
-        series_key = getattr(ds, "SeriesInstanceUID")
+        patient_id = reader.GetMetaData("0010|0020")
         
-        if not series_key in series_paths:
-            series_paths[series_key] = []
-            
-        series_paths[series_key].append(old_path)
-
-    for series_uid, paths in series_paths.items():
-        if args.append_study_uid:
-            patient_id += "_" + study_uid
-            
-        series_dir = Path(out_directory, patient_id, series_uid)
-        os.makedirs(series_dir, exist_ok=True)
+        # skip series with contrast applied tag
+        if reader.HasMetaDataKey("0018|0010"):
+            contrast = reader.GetMetaData("0018|0010")
+            if contrast:
+                contrast = contrast.lower()
+                if "no" not in contrast:
+                    continue 
         
-        for src_path in paths:
-            dest_path = Path(series_dir, src_path.stem)
-            shutil.copy2(src_path, dest_path)
-        total_files = sum([len(l) for l in series_paths.values()])
-    print(f"moved ID {patient_id}, {total_files} files to {Path(out_directory, patient_id)}")
+        if reader.HasMetaDataKey("0018|0050"):
+            slice_thickness = reader.GetMetaData("0018|0050")
+            if slice_thickness:
+                filtered_series[series_id] = {
+                    'slice_thickness': float(slice_thickness),
+                    'filenames': filenames, 
+                    'patient_id': patient_id
+                    }
+        
+    selected_uid = min(filtered_series, key=lambda uid: filtered_series[uid]['slice_thickness'])
+    selected_series = filtered_series[selected_uid]
+    
+    print(f"selected series: {selected_uid} with slice thickness: {selected_series['slice_thickness']}, files: {len(selected_series['filenames'])}")
+        
+    output_directory = Path(args.out_directory, selected_series['patient_id'])
+    os.makedirs(output_directory)
+    
+    moved_files = 0
+    for src_path in selected_series['filenames']:
+        src_path = Path(src_path).absolute()
+        dest_path = Path(output_directory, src_path.stem).absolute()
+        
+        try:
+            shutil.copy2(src=src_path, dst=dest_path)
+            moved_files += 1
+        except Exception as exc:
+            print(exc)
+        
+    print(f"moved {moved_files}/{len(selected_series['filenames'])} files")
         
 
         
